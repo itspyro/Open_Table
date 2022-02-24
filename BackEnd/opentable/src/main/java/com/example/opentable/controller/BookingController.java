130f36c70d11cf0b5f558b4141cec62b6630d599
Dushyant.Sharma <dushyant.sharma@PSMAC-GGN-082.local>
2022-02-24 22:42:58 +0530

    booking

--- BackEnd/opentable/src/main/java/com/example/opentable/controller/BookingController.java
+++ BackEnd/opentable/src/main/java/com/example/opentable/controller/BookingController.java
@@ -1,10 +1,71 @@
 package com.example.opentable.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.opentable.service.BookingService;
+import com.example.opentable.transport.BookingDetailsResponse;
+import com.example.opentable.transport.ResponseMessage;
+import com.example.opentable.transport.dto.CreateBookingDto;
+
 @RestController
-@RequestMapping("/api/user/{uesrid}/booking")
+@RequestMapping("/api/bookings")
 public class BookingController {
+	
+	@Autowired
+	BookingService bookingService;
+	
+	@GetMapping("/user/{id}")
+	public ResponseEntity<BookingDetailsResponse> getAllBookingsByUser(@PathVariable(value = "id") int userId)
+	{
+		BookingDetailsResponse response = new BookingDetailsResponse();
+		try
+		{
+			response.setBookings(bookingService.getAllBookingsByUser(userId));
+			response.setHttpStatusCode(HttpStatus.OK.value());
+			response.setResponseMessage("Successful");
+		}
+		
+		catch(Exception e)		{
+			response.setBookings(null);
+			response.setHttpStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+			response.setResponseMessage(e.getMessage());
+		}
+		
+		return new ResponseEntity<BookingDetailsResponse>(response, HttpStatus.OK);
+		
+	}
+	
+	@PostMapping("/create")
+	public ResponseEntity<ResponseMessage> createBooking(@RequestBody CreateBookingDto createBookingDto)
+	{
+		int bookingId;
+		ResponseMessage response = new ResponseMessage();
+		try
+		{
+			bookingId = bookingService.createBooking(createBookingDto);
+			response.setResponseMessage(String.format("Booking with id %d is created", bookingId));
+			response.setHttpStatusCode(HttpStatus.OK.value());
+		}
+		
+		catch(Exception e)
+		{
+			response.setResponseMessage(e.getMessage());
+			response.setHttpStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+			return new ResponseEntity<ResponseMessage> (response,HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		return new ResponseEntity<ResponseMessage> (response,HttpStatus.OK);
+	}
+	
+	
+	
 
 }
--- BackEnd/opentable/src/main/java/com/example/opentable/service/impl/BookingServiceImpl.java
+++ BackEnd/opentable/src/main/java/com/example/opentable/service/impl/BookingServiceImpl.java
@@ -1,12 +1,34 @@
 package com.example.opentable.service.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.opentable.repository.dao.BookingDao;
 import com.example.opentable.service.BookingService;
+import com.example.opentable.transport.dto.BookingDto;
+import com.example.opentable.transport.dto.CreateBookingDto;
 
 
 @Service
 public class BookingServiceImpl implements BookingService {
 	
+	@Autowired
+	BookingDao bookingDao;
+	
 
+	@Override
+	public List<BookingDto> getAllBookingsByUser(int userId) throws Exception {
+		
+		return bookingDao.getAllBookingsByUser(userId);
+	}
+
+	@Override
+	public int createBooking(CreateBookingDto createBookingDto) throws Exception {
+		return bookingDao.createBooking(createBookingDto);
+	}
+	
+	
+	
 }
