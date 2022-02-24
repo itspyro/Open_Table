4c06f16772838320ad505ffb8e74320bfc25effd
kks1111 <kalpeshkumarsinghaum@gmail.com>
2022-02-24 15:29:24 +0530

    update user details response

--- BackEnd/opentable/src/main/java/com/example/opentable/transport/UserDetailsResponse.java
+++ BackEnd/opentable/src/main/java/com/example/opentable/transport/UserDetailsResponse.java
@@ -3,6 +3,7 @@ package com.example.opentable.transport;
 import java.io.Serializable;
 import java.util.List;
 
+import com.example.opentable.transport.dto.RestaurantDto;
 import com.example.opentable.transport.dto.UserDto;
 
 public class UserDetailsResponse extends ResponseMessage implements Serializable {
@@ -13,6 +14,8 @@ public class UserDetailsResponse extends ResponseMessage implements Serializable
 	private static final long serialVersionUID = -2510215621076508764L;
 	
 	List<UserDto> users;
+	
+	List<RestaurantDto> restaurants;
 
 	public UserDetailsResponse() {
 		
@@ -24,6 +27,14 @@ public class UserDetailsResponse extends ResponseMessage implements Serializable
 
 	public void setUsers(List<UserDto> users) {
 		this.users = users;
+	}
+
+	public List<RestaurantDto> getRestaurants() {
+		return restaurants;
+	}
+
+	public void setRestaurants(List<RestaurantDto> restaurants) {
+		this.restaurants = restaurants;
 	}
 
 }
