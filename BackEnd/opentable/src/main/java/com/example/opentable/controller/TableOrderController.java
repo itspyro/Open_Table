1e366fcad4da6b120517030d763c1e521a53bae5
Aakriti Sahrawat <sahrawataakriti2@gmail.com>
2022-02-28 22:46:55 +0530

    changed mapping to post & debugged

--- BackEnd/opentable/src/main/java/com/example/opentable/controller/TableOrderController.java
+++ BackEnd/opentable/src/main/java/com/example/opentable/controller/TableOrderController.java
@@ -3,7 +3,7 @@ package com.example.opentable.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +19,7 @@ public class TableOrderController {
 @Autowired
 TableOrderService tableOrderService;
 	
- @GetMapping("/checkAvailability/")
+ @PostMapping("/checkAvailability/")
  public ResponseEntity<BenchDetailsResponse> checkAvailability (@RequestBody CheckAvailabilityDto checkAvailabilityDto)
  {
 	 BenchDetailsResponse response = new BenchDetailsResponse();
